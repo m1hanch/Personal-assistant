bbc5ee559adfa6548ef8bea8c9c794ab3c31c3d4
m1hanch <misha5264@gmail.com>
2024-01-11 15:50:40 +0100

    project_structure

---
+++ personal_assistant/app_auth/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'app_auth'
+
+urlpatterns = [
+    #path('', views.index, name='index'),
+]
---
+++ personal_assistant/file_manager/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'file_manager'
+
+urlpatterns = [
+    #path('', views.index, name='index'),
+]
