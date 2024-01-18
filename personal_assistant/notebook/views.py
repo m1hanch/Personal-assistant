b998f076ca3a5e122886ddb7b002e3b85e93ad9b
a <likafloat@gmail.com>
2024-01-18 04:13:11 +0200

    редагування та пошук

--- personal_assistant/notebook/views.py
+++ personal_assistant/notebook/views.py
@@ -49,3 +49,33 @@ def detail(request, note_id):
 def delete_note(request, note_id):
     Note.objects.get(pk=note_id).delete()
     return redirect(to='notebook:main')
+
+# Додавання функціоналу пошуку
+def search_notes(request):
+    query = request.GET.get('query', '')
+    notes = Note.objects.filter(name__icontains=query)
+    return render(request, 'notebook/search_results.html', {"notes": notes, "query": query})
+
+# редагування
+def edit_note(request, note_id):
+    note = get_object_or_404(Note, pk=note_id)
+    
+    # Отримуємо всі теги для передачі у форму
+    all_tags = Tag.objects.all()
+    
+    if request.method == 'POST':
+        form = NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            edited_note = form.save(commit=False)
+            edited_note.tags.clear()  
+            choice_tags = Tag.objects.filter(name__in=request.POST.getlist('tags'))
+            for tag in choice_tags.iterator():
+                edited_note.tags.add(tag)
+            edited_note.save()
+            return redirect(to='notebook:main')
+        else:
+            return render(request, 'notebook/edit_note.html', {"note": note, 'form': form, 'all_tags': all_tags})
+    else:
+        form = NoteForm(instance=note)
+        return render(request, 'notebook/edit_note.html', {"note": note, 'form': form, 'all_tags': all_tags})
+
