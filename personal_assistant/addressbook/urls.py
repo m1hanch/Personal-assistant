2d8110cdfda49ae29a334b1b89a5ed162f1aff68
AB-GAMES\andrey.vlasiuk <andrey.vlasiuk@ab-games.com>
2024-01-21 19:34:04 +0200

    Add edit_contact, delete_contact, search

--- personal_assistant/addressbook/urls.py
+++ personal_assistant/addressbook/urls.py
@@ -7,4 +7,6 @@ app_name = 'addressbook'
 urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add_contact, name='add_contact'),
+    path('edit_contact/<int:contact_id>/', views.edit_contact, name='edit_contact'),
+    path('delete/<int:contact_id>/', views.delete_contact, name='delete_contact'),
 ]
--- personal_assistant/addressbook/views.py
+++ personal_assistant/addressbook/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.db.models import Q
 from .forms import ContactForm
 from .models import Contact
 
 
 # Create your views here.
 def index(request):
-    contacts = Contact.objects.filter(user=request.user) # noqa
+    contacts = Contact.objects.filter(user=request.user)
+
+    query = request.GET.get('q')
+    if query:
+        contacts = contacts.filter(Q(first_name__icontains=query) |
+                                   Q(last_name__icontains=query) |
+                                   Q(phone__icontains=query))
+
     return render(request, 'addressbook/index.html', context={'contacts': contacts})
+
 
 def add_contact(request):
     contacts = Contact.objects.filter(user=request.user)  # noqa
@@ -18,5 +26,30 @@ def add_contact(request):
             new_contact.user_id = request.user.id
             form.save()
         else:
-            return render(request, 'addressbook/index.html', context={'form': form,'contacts': contacts})
-    return redirect(to='addressbook:index')
+            return render(request, 'addressbook/index.html', context={'form': form, 'contacts': contacts})
+    return redirect('addressbook:index')
+
+
+def edit_contact(request, contact_id):
+    if request.user.is_authenticated:
+        contact = get_object_or_404(Contact, id=contact_id, user=request.user)
+        if request.method == 'POST':
+            form = ContactForm(request.POST, instance=contact)
+            if form.is_valid():
+                form.save()
+                return redirect('addressbook:index')
+        else:
+            form = ContactForm(instance=contact)
+        return render(request, 'addressbook/edit_contact.html', {'form': form, 'contact': contact})
+    else:
+        return redirect('addressbook:index')
+
+
+def delete_contact(request, contact_id):
+    contact = get_object_or_404(Contact, id=contact_id, user=request.user)
+
+    if request.method == 'POST':
+        contact.delete()
+        return redirect('addressbook:index')
+
+    return render(request, 'addressbook/confirm_delete.html', {'contact': contact})
